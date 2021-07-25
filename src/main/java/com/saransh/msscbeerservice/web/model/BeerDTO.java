84ea80e7690ec3cfe0d94f81b070dffff30ca8ec
Saransh Kumar <cryptosingh@gmail.com>
2021-07-25 18:36:54 +0530

    Closes #8: Add Bean Validation on Beer DTO object.

--- src/main/java/com/saransh/msscbeerservice/web/model/BeerDTO.java
+++ src/main/java/com/saransh/msscbeerservice/web/model/BeerDTO.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -17,13 +21,31 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDTO {
+    @Null
     private UUID id;
-    private String beerName;
-    private BeerStyle beerStyle;
-    private Long upc;
-    private BigDecimal price;
-    private Integer quantityOnHand;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @Null
     private Integer version;
+
+    @NotBlank
+    private String beerName;
+
+    @NotNull
+    private BeerStyle beerStyle;
+
+    @Positive
+    @NotNull
+    private Long upc;
+
+    @Positive
+    @NotNull
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
 }
