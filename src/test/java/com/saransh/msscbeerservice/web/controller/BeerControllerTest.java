e60232d5aa36682f9bf419413f04df3950f64d1f
Saransh Kumar <cryptosingh@gmail.com>
2021-07-26 20:06:56 +0530

    Closes #11: Fix Failing Tests.

--- src/test/java/com/saransh/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/saransh/msscbeerservice/web/controller/BeerControllerTest.java
@@ -2,12 +2,14 @@ package com.saransh.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.saransh.msscbeerservice.web.model.BeerDTO;
+import com.saransh.msscbeerservice.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -32,7 +34,7 @@ class BeerControllerTest {
 
     @Test
     void saveBeer() throws Exception {
-        BeerDTO beerDTO = BeerDTO.builder().build();
+        BeerDTO beerDTO = getValidBeerDTO();
         String beerJson = objectMapper.writeValueAsString(beerDTO);
         mockMvc.perform(
                 post("/api/v1/beer")
@@ -43,12 +45,20 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDTO beerDTO = BeerDTO.builder().build();
+        BeerDTO beerDTO = getValidBeerDTO();
         String beerJson = objectMapper.writeValueAsString(beerDTO);
         mockMvc.perform(
                 put("/api/v1/beer/" + uuid)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerJson))
                 .andExpect(status().isOk());
+    }
+
+    BeerDTO getValidBeerDTO() {
+        return BeerDTO.builder()
+                .beerName("My Beer")
+                .beerStyle(BeerStyle.PILSNER)
+                .price(new BigDecimal("14.12"))
+                .upc(1234567890L).build();
     }
 }
