676e7358737e8abdd7a3b92aea9e4d110f9bd0c3
Saransh Kumar <cryptosingh@gmail.com>
2021-08-09 19:27:30 +0530

    Closes #19: Fix BeerController Test.

--- src/test/java/com/saransh/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/saransh/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,11 +1,13 @@
 package com.saransh.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.saransh.msscbeerservice.service.BeerService;
 import com.saransh.msscbeerservice.web.model.BeerDTO;
 import com.saransh.msscbeerservice.web.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -18,17 +20,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
+    private final UUID uuid = UUID.randomUUID();
     @Autowired
     private MockMvc mockMvc;
     @Autowired
     private ObjectMapper objectMapper;
-    private final UUID uuid = UUID.randomUUID();
+    @MockBean
+    private BeerService beerService;
 
     @Test
     void getBeerById() throws Exception {
         mockMvc.perform(
-                get("/api/v1/beer/" + uuid)
-                        .accept(MediaType.APPLICATION_JSON))
+                        get("/api/v1/beer/" + uuid)
+                                .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
@@ -37,9 +41,9 @@ class BeerControllerTest {
         BeerDTO beerDTO = getValidBeerDTO();
         String beerJson = objectMapper.writeValueAsString(beerDTO);
         mockMvc.perform(
-                post("/api/v1/beer")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(beerJson))
+                        post("/api/v1/beer")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(beerJson))
                 .andExpect(status().isCreated());
     }
 
@@ -48,9 +52,9 @@ class BeerControllerTest {
         BeerDTO beerDTO = getValidBeerDTO();
         String beerJson = objectMapper.writeValueAsString(beerDTO);
         mockMvc.perform(
-                put("/api/v1/beer/" + uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(beerJson))
+                        put("/api/v1/beer/" + uuid)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(beerJson))
                 .andExpect(status().isOk());
     }
 
